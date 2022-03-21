b7ec2bb26a84c61dac9dd70760934e1730f45bf4
bbbkim <rtq0427@gmail.com>
2022-03-21 14:17:26 +0900

    pip

---
+++ python_tutorial/4_1_number.py
@@ -0,0 +1,13 @@
+print(-1)
+print(0)
+print(1) #int
+print(1.1) #float
+print(1+1)
+print('1+1')
+
+import math
+print('math.sqrt(4)', math.sqrt(4))
+print(math.pow(4,2))
+
+import random
+print(random.random())
---
+++ python_tutorial/4_2_text.py
@@ -0,0 +1,11 @@
+print('hello world')
+print("hello world")
+print('''
+hello
+world
+''')
+
+print("'1'+'1'",'1'+'1')
+print('hello world'*10)
+print(len('hello world'*10))
+print('hello world'.replace('world','universe'))
